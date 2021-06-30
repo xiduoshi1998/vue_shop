ec9b424702850f99029eb21bc4f00ca330684a60
xiduoshi-glb <xiduoshi-glb@qq.com>
2021-07-01 00:20:01 +0800

    完成分类参数

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ const Users = () => import('components/user/Users')
 const Rights = () => import('components/power/Rights')
 const Roles = () => import('components/power/Roles')
 const Cate = () => import('components/goods/Cate')
+const Params = () => import('components/goods/Params')
 
 Vue.use(VueRouter)
 
@@ -44,6 +45,10 @@ const routes = [
       {
         path: '/categories',
         component: Cate
+      },
+      {
+        path: '/params',
+        component: Params
       }
     ]
   },
